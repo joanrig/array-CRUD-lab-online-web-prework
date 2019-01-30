e931b628183b6fd31dc11a4337a236a1a50fc90d
Joan in DC <joanrigdon@gmail.com>
2019-01-30 23:39:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array << element
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift element
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
@@ -21,15 +21,40 @@ end
 def remove_element_from_start_of_array(array)
   array.shift
 end
+  
 
-def retrieve_element_from_index(array, index_number)
-  array[index_number]
-end
+# def create_an_empty_array
+#   []
+# end
 
-def retrieve_first_element_from_array(array)
-  array[0]
-end
+# def create_an_array
+#   [1,2,3,4]
+# end
 
-def retrieve_last_element_from_array(array)
-  array.last
-end
+# def add_element_to_end_of_array(array, element)
+#   array << element
+# end
+
+# def add_element_to_start_of_array(array, element)
+#   array.unshift element
+# end
+
+# def remove_element_from_end_of_array(array)
+#   array.pop
+# end
+
+# def remove_element_from_start_of_array(array)
+#   array.shift
+# end
+
+# def retrieve_element_from_index(array, index_number)
+#   array[index_number]
+# end
+
+# def retrieve_first_element_from_array(array)
+#   array[0]
+# end
+
+# def retrieve_last_element_from_array(array)
+#   array.last
+# end
